0254e6d14567ed25bd533337616470e0e76ceb1f
khanabuzar010 <64134243+khanabuzar010@users.noreply.github.com>
2020-10-23 14:28:52 +0530

    Adding Task

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Todo extends LightningElement {}
+export default class Todo extends LightningElement {
+
+    @track
+    todoTasks = [];
+
+    newTask = '';
+
+    updateNewTask(event) {
+        this.newTask = event.target.value;
+    }
+
+    addTaskToList(event) {
+    
+        this.todoTasks.push({
+            id: this.todoTasks.length + 1,
+            name: this.newTask
+        });
+        this.newTask = '';
+    }
+
+}
